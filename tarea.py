2befcf231564a7d505a1137131eee47523771d8a
Jael <jaelbela36@gmail.com>
2024-08-01 08:39:39 -0300

    first commit

---
+++ tarea.py
@@ -0,0 +1,8 @@
+largo = int(input("Ingrese el largo del rectángulo"))
+ancho = int(input("Ingrese el ancho del ractángulo"))
+area = largo * ancho
+print(f"Su area es de {area}")
+perimetro = largo * 2 + ancho *2 
+print(f"Su perimetro es de {perimetro}")
+tupla = (area, perimetro)
+print(tupla)
